bfdd6523f681345d09fba03967f856934790c7ac
Claudia Kruger <claudiagracieli@gmail.com>
2022-02-24 18:45:50 -0300

    ifs

--- script.js
+++ script.js
@@ -13,6 +13,17 @@ function showTime() {
     let h = d.getHours();
     let m = d.getMinutes();
     let s = d.getSeconds();
+
+    if (h < 10) {
+        h = "0"+h;
+    }
+    if (m < 10) {
+        m = "0"+m;
+    }
+    if (s < 10) {
+        s = "0"+s;
+    }
+    
     let text = `${h}:${m}:${s}`;
 
     document.querySelector('.demo').innerHTML = text;
